10a599cddfb2bb29739732d5c2aa19c84119f0de
Chase Brashear <chase.brashear@gmail.com>
2021-07-28 15:59:04 -0700

    updating nav

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,9 +1,11 @@
 
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 
 const NavBar = () => {
+  const user = useSelector(state => state.session.user)
 
   return (
     <nav className='navbar navbar-expand-lg navbar-light bg-light'>
@@ -11,16 +13,21 @@ const NavBar = () => {
         <NavLink to='/' exact={true} activeClassName='active'>
           Home
         </NavLink>
-        <NavLink to='/login' exact={true} activeClassName='active'>
-          Login
-        </NavLink>
-        <NavLink to='/sign-up' exact={true} activeClassName='active'>
-          Sign Up
-        </NavLink>
-        <NavLink to='/users' exact={true} activeClassName='active'>
-          Users
-        </NavLink>
-        <LogoutButton />
+        {(user) ? (
+          <LogoutButton />
+        ) : (
+          <div>
+            <NavLink to='/login' exact={true} activeClassName='active'>
+              Login
+            </NavLink>
+            <NavLink to='/sign-up' exact={true} activeClassName='active'>
+              Sign Up
+            </NavLink>
+            {/* <NavLink to='/users' exact={true} activeClassName='active'>
+              Users
+            </NavLink> */}
+          </div>
+        )}
       </div>
     </nav>
   );
