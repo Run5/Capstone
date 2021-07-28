fcb95d9e2dae142893947437a6653ece9dd2b546
Chase Brashear <chase.brashear@gmail.com>
2021-07-28 15:02:38 -0700

    add main normalized

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,10 +1,9 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 
 const NavBar = () => {
-  // const [collapse, setCollapse] = useState('true')
 
   return (
     <nav>
