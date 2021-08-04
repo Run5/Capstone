f35bdef31e547da067d467fa02f0a51caa1453d9
Chase Brashear <chase.brashear@gmail.com>
2021-08-04 10:19:22 -0700

    add navbar styling

--- react-app/src/components/Home/Splash.js
+++ react-app/src/components/Home/Splash.js
@@ -6,7 +6,7 @@ export default function Splash() {
   return (
     <div>
       <h1>Modest</h1>
-      <video id="videoTag" muted="muted" preload="none" loop="loop" data-resize="true" autoPlay="">
+      <video id="videoTag" muted="muted" preload="none" loop="loop" data-resize="true" autoplay="">
         <source className="webmSrc" type="video/webm" src="https://s1.pearlcdn.com/global_static/video/bdc_sand_loop.webm" />
         <source className="mp4Src" type="video/mp4" src="https://s1.pearlcdn.com/global_static/video/bdc_sand_loop.mp4" />
       </video>
