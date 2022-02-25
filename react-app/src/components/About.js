40735301e26d3159d374e70943d4d032ff45c529
Chase Brashear <chase.brashear@gmail.com>
2022-02-24 20:48:18 -0800

    add updates to about

--- react-app/src/components/About.js
+++ react-app/src/components/About.js
@@ -8,7 +8,8 @@ export default function About() {
       <div className='about-container'>
         <h1 className='about-title'>Modest Grind Sessions</h1>
         <p className='about-p'>
-          Modest grind sessions was created to track your grind sessions.
+          Modest grind sessions was created to track your grind sessions in the MMO Black Desert Online.
+          The objective of the game is to kill mobs for hours at a time and this site was created to help track the spoils from this.
           Users can create characters to more easily track their grind.
           Grind sessions can be tracked for any high-end grind location.
         </p>
